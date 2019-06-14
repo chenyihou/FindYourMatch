8f854881ace7e7351c8bc13c6d809a47a1064abd
Chenyi Hou <hcy13916978481@gmail.com>
2019-06-13 20:49:52 -0400

    forg

--- functions/index.js
+++ functions/index.js
@@ -15,9 +15,24 @@ admin.initializeApp({
 });
 
 var db = admin.firestore();
-
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
+
+var docRef = db.collection('users').doc('alovelace');
+
+var setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
+
+exports.queue=functions.https.onCall((data,context)=>{
+  var setAda = docRef.set({
+    username:data
+  });
+  
+})
+
 exports.hello = functions.https.onCall((data, context) => {
   console.log('helloworld')
   return {
